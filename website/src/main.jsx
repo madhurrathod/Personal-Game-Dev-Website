a01dc8d06c216ecbfbc7a7b4e38066fd8a5c45a0
Madhur Rathod <madhur.thanku@gmail.com>
2025-04-19 02:26:27 +0530

    removed useless files

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Home } from './home'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
